a82845786e000a75aa88c1fbc051209a338c39dc
anotherdev <github.com@anotherdev.com>
2018-03-16 16:28:54 +0700

    add api key

---
+++ app/src/main/java/com/ekoapp/simplechat/SimpleChatApp.java
@@ -0,0 +1,14 @@
+package com.ekoapp.simplechat;
+
+import android.app.Application;
+
+import com.ekoapp.ekosdk.EkoChat;
+
+public class SimpleChatApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        EkoChat.setup("b3bde15c3989f86045658e4a530a1688d1088be0be3d6f25");
+    }
+}
