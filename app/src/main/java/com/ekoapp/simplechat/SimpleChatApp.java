16b9b86e10663e56d3af0ab099edc798e224ac5f
anotherdev <polson.k+github@gmail.com>
2018-03-21 04:31:25 +0700

    EkoChat renamed to EkoClient

--- app/src/main/java/com/ekoapp/simplechat/SimpleChatApp.java
+++ app/src/main/java/com/ekoapp/simplechat/SimpleChatApp.java
@@ -2,13 +2,13 @@ package com.ekoapp.simplechat;
 
 import android.app.Application;
 
-import com.ekoapp.ekosdk.EkoChat;
+import com.ekoapp.ekosdk.EkoClient;
 
 public class SimpleChatApp extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-        EkoChat.setup("b3bde15c3989f86045658e4a530a1688d1088be0be3d6f25");
+        EkoClient.setup("b3bde15c3989f86045658e4a530a1688d1088be0be3d6f25");
     }
 }
