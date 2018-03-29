53125c4b9f202ff5ee01a9d281a9caa3638234a6
anotherdev <github.com@anotherdev.com>
2018-03-29 17:29:26 +0700

    add menu to access ChatKit

--- app/src/main/java/com/ekoapp/simplechat/ChannelListActivity.java
+++ app/src/main/java/com/ekoapp/simplechat/ChannelListActivity.java
@@ -1,5 +1,6 @@
 package com.ekoapp.simplechat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,6 +14,7 @@ import com.afollestad.materialdialogs.MaterialDialog;
 import com.ekoapp.ekosdk.EkoChannel;
 import com.ekoapp.ekosdk.EkoChannelRepository;
 import com.ekoapp.ekosdk.EkoClient;
+import com.ekoapp.simplechat.chatkit.ChatKitChannelListActivity;
 
 import butterknife.BindView;
 import io.reactivex.Completable;
@@ -70,6 +72,9 @@ public class ChannelListActivity extends BaseActivity {
                     })
                     .show();
             return true;
+        } else if (id == R.id.action_chatkit) {
+            Intent chatKit = new Intent(this, ChatKitChannelListActivity.class);
+            startActivity(chatKit);
         }
         return super.onOptionsItemSelected(item);
     }
